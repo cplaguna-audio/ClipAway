d0f485c93b2deac3d7d67556e8a7dd487bb7ccc0
Christopher Laguna <cplaguna37@gmail.com>
2016-08-27 14:11:06 -0400

    Adding MIT license

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ ClipAway
 
 ClipAway is a web application for declipping audio. Processing occurs in the
 browser, implemented in JavaScript.
+
+This project is licensed under the MIT license. See LICENSE.txt.
--- js/DetectClipping.js
+++ js/DetectClipping.js
@@ -1,3 +1,29 @@
+/*
+ * ClipAway
+ *
+ * Copyright (c) 2016 Christopher Laguna
+ * https://github.com/cplaguna-audio/ClipAway
+ *
+ * (MIT License)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /*****************************************************************************\
  *                             DetectClipping.js                             *
  *                                                                           *
@@ -195,8 +221,12 @@ function DetectClipping(x, channel_idx, params) {
 
   // Aggregate the positive and negative clipping intervals.
   var clip_intervals = [];
-  clip_intervals.push.apply(clip_intervals, negative_clip_intervals);
-  clip_intervals.push.apply(clip_intervals, positive_clip_intervals);
+  for(var clip_interval_idx = 0; clip_interval_idx < negative_clip_intervals.length; clip_interval_idx++) {
+    clip_intervals.push(negative_clip_intervals[clip_interval_idx]);
+  }
+  for(var clip_interval_idx = 0; clip_interval_idx < positive_clip_intervals.length; clip_interval_idx++) {
+    clip_intervals.push(positive_clip_intervals[clip_interval_idx]);
+  }
   clip_intervals = MergeIntervals(clip_intervals, 1);
 
   // console.log('Negative level: ' + negative_thresh + ', negative width: ' + negative_width); 
